02ea897a47cd1269d6ef5e724a1085aff9e746e8
Samer Ghaly <samereaghaly@gmail.com>
2021-11-03 18:26:23 +0200

    Add API credentials and base URL

--- website/app.js
+++ website/app.js
@@ -1,5 +1,6 @@
 /* Global Variables */
-
+const apiKey = "6e17373dacbbe7e9393c4a937ee7664b";
+const baseURL = "http://api.openweathermap.org/data/2.5/weather?zip=";
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
